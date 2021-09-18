eafc5f359e12c84d808852f4cadc4432248d9a73
JJefro <jevjefro@gmail.com>
2021-09-19 00:01:58 +0300

    Create .gitignore and .swiftlint.yml

---
+++ textFields!/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  textFields!
+//
+//  Created by Jevgenijs Jefrosinins on 18/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
