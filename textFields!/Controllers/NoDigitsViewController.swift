2eac053f23cccb888f8aea38ce7019cb3bfd1d6c
JJefro <jevjefro@gmail.com>
2021-09-29 12:42:47 +0300

    make stateful elements

--- textFields!/Controllers/NoDigitsViewController.swift
+++ textFields!/Controllers/NoDigitsViewController.swift
@@ -11,13 +11,9 @@ class NoDigitsViewController: UIViewController {
 
     @IBOutlet weak var noDigitsField: TextFieldView!
 
-    var model = TextFieldModel()
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        noDigitsField.txtField.delegate = self
-        model.fieldSettings = .noDigits
         noDigitsField.fieldSettings = .noDigits
     }
 }
